ba9a360d2b4a4afe19cbdeae0d9920e8add6cde5
elladqa <66439799+elladqa@users.noreply.github.com>
2020-08-08 16:35:57 +0300

    Create task6.cpp

---
+++ task6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+    int main ()
+{
+
+        int a , b;
+         std::cin >> a >> b;
+
+         int a = 0;
+         int b = 0;
+
+         int c = a / b;
+         std::cout << c << std:: endl;
+
+
+    }
+
+       return 0;
